72194c57f6e90b846abb1a7e8d17ade1f1bf208b
Andy Soell <andy@teamsoell.com>
2015-07-01 10:50:06 -0400

    Split events into Upcoming and Past categories

--- page-events.php
+++ page-events.php
@@ -13,25 +13,77 @@
 get_header();
 
 $Q = new GetPostsQuery();
-$args = array();
-$args = Array(
-  'post_type' => 'events',
-  'order' => 'DESC',
-  'orderby' => 'date');
-$events = $Q->get_posts($args);
 
+$args = Array(
+  'post_type'   => 'events',
+  'order'       => 'ASC',
+  'orderby'     => 'cdate');
+
+$events = $Q->get_posts($args);
+$upcomingEvents = "";
+$pastEvents = "";
 ?>
       <section id="events">
-<? foreach ($events as $event) : if (! is_null($event)) : ?>
-        <article>
-          <?php echo wp_get_attachment_image($event['fullsize'], 'full'); ?>
-          <h1 class="entry-title"><?php echo $event['post_title']; ?></h1>
-          <div class="entry-meta"><?php echo $event['date'];?><?php if (strlen($event['enddate'])>0) : ?> &ndash; <?php echo $event['enddate']; ?><?php elseif (isset($event['time'])):?> &mdash; <?php echo $event['time']; ?><?php endif; ?><?php if (isset($event['venue'])) : ?> at <?php echo $event['venue']; ?><?php endif; ?></div>
-          <aside><?php echo $event['summary']; ?></aside>
-          <?php if (strlen($event['link'])>0) : ?>
-            <p class="footnote">For more information visit <a href="<?php echo $event['link']; ?>"><?php echo $event['link']; ?></a></p>
-          <?php endif; ?>
-        </article>
-<?php endif; endforeach; ?>
+<?
+  foreach ($events as $event) {
+    if (! is_null($event)) {
+      if (strlen($event['enddate'])>0) {
+        // has an end date
+        if (date("F Y", strtotime($event['date']))==date("F Y", strtotime($event['enddate']))) {
+          // same month
+          $event_displaydate = date("F j", strtotime($event['date'])) .'&ndash;'.date("j, Y", strtotime($event['enddate']));
+        } else {
+          // different month
+          $event_displaydate = date("F j", strtotime($event['date'])) .'&ndash;'.date("F j, Y", strtotime($event['enddate']));
+        }
+      } else {
+        $event_displaydate = date("F j, Y", strtotime($event['date']));
+      }
+
+      $event_fullsize = wp_get_attachment_image($event['fullsize'], 'full');
+      $event_title = $event['post_title'];
+      $event_summary = $event['summary'];
+      $event_timestamp = strtotime($event['date']);
+      if (strlen($event['venue'])>0) {
+        $event_venue = "&bullet; " . $event['venue'] . "<br />";
+      } else {
+        $event_venue = "";
+      }
+      if (strlen($event['time'])>0) {
+        $event_time = "&bullet; " . $event['time'] . "<br />";
+      } else {
+        $event_time = "";
+      }
+      if (strlen($event['link'])>0) {
+        $event_link = '<p class="footnote">For more information visit <a href="'.$event['link'].'">'.$event['link'].'</a></p>';
+      } else {
+        $event_link = "";
+      }
+      $event_layout = <<<EOF
+          <article>
+            $event_fullsize
+            <h1 class="entry-title">$event_title</h1>
+            <div class="entry-meta">$event_displaydate<br />
+            $event_venue
+            $event_time
+            <aside>$event_summary</aside>
+            $event_link
+          </article>
+EOF;
+      if (strtotime($event['date'])>time()) {
+        // future event
+        $upcomingEvents .= $event_layout;
+      } else {
+        $pastEvents = $event_layout.$pastEvents;
+      }
+    }
+  }
+
+print '<h1>Upcoming events</h1>';
+print $upcomingEvents;
+print '<br /><br /><hr style="clear:both; margin-top:20px;" />';
+print '<h1>Past events</h1>';
+print $pastEvents;
+?>
       </section>
 <?php get_footer(); ?>
